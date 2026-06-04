d8af7d931b331345e146e0b53c170b6fa6109306
naite-101 <2236980615@qq.com>
2026-06-04 13:06:21 +0800

    修改日志中snprint这个函数的参数问题，以及修复函数名的拼写错误

--- src/connection.h
+++ src/connection.h
@@ -1,9 +1,9 @@
 #ifndef CONNECTION_H
 #define CONNECTION_H
 
-//处理接受连接
-void handle_accept(int listen_fd,int epfd);
-//读取客户端的数据
-void handle_clinet_data(int client_fd,int epfd);
+// 处理接受连接
+void handle_accept(int listen_fd, int epfd);
+// 读取客户端的数据
+void handle_client_data(int client_fd, int epfd);
 
 #endif
